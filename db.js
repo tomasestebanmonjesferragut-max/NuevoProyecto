c6a5c3c0d42bbb3835ed7e062e12e99b44a45609
Zommy <tomasestebanmonjesferragut@gmail.com>
2026-08-27 14:41:54 -0400

    tf

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'asistencia_db',
     waitForConnections: true,
     connectionLimit: 10,
